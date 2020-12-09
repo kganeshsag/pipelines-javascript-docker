d7575b3989cf4e5f99d0bcef7208c89a51c8fd6c
kganeshsag <75366582+kganeshsag@users.noreply.github.com>
2020-12-09 10:40:26 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world from Azure\n');
+  res.send('Hello world from Azure updated\n');
 });
 
 app.listen(PORT, HOST);
